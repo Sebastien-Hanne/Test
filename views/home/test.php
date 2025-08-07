679b72f43de7964088b9bd0001d72fa6203790f2
Sébastien <hannesebastien@gmail.com>
2025-08-07 13:24:54 +0200

    LOOOOL

---
+++ views/home/test.php
@@ -0,0 +1,10 @@
+<div class="page-header">
+    <div class="container">
+        <h1><?php e($title); ?></h1>
+    </div>
+</div>
+
+<section class="content">
+    <div class="container">
+    </div>
+</section> 
